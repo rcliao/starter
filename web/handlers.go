c2bf84119b5e720e7bf7d23bf5f678237ece6540
Eric Liao <rcliao01@gmail.com>
2016-10-20 01:14:25 -0700

    fix handler write for hello world

--- web/handlers.go
+++ web/handlers.go
@@ -7,7 +7,7 @@ import (
 
 // Index renders the hello world message
 func Index(client *Client) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Print(w, "Hello GoLang web starter!")
-	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello GoLang web starter!")
+	})
 }
